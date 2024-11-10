ff42e3373adc7b69e492a43c8b8303c55ed9100c
Timo Pährisch <timo.paehrisch@icloud.com>
2024-11-10 23:25:56 +0100

    Fixed tests

--- bitcoinproxy/proxy.py
+++ bitcoinproxy/proxy.py
@@ -181,7 +181,7 @@ class BTCProxy:
         LOG.info(f"Dest URL is {destipadress}:{destportnumber}")
 
         async with session.post(url, json={"method": method, "params": params}) as response:
-            resp_json = await response.json()
+#            resp_json = await response.json()
             data = await response.text()
             LOG.info(f"Response for forwarded request: {method}: {data[:200]}...{data[-200:]}")
             return response
